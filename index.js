f3783380f62927f9eccf3c3f14bc4d6b64269c69
Sriram <santhanamsriram07@gmail.com>
2021-09-01 09:31:05 +0530

    revised changes

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
-const {mergeVideoAudio, createDataArray} = require('./helper.js')
+const {mergeVideoAudio, createDataArray} = require('./helper.js');
 const express = require('express');
-const ytdl = require('ytdl-core')
+const ytdl = require('ytdl-core');
+const path = require('path');
 
 const app = express()
 app.use(express.urlencoded({extended:true}));
 app.use(express.static('public'));
+app.set('views', path.join(__dirname, '/views'))
 app.use(express.json());
 
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-    res.render('index')
+    console.log('working')
+    res.render('index') 
 })
 
 app.post('/done', async (req, res) => {
     const link = req.body.search;
+
+    if (!ytdl.validateURL(link)){
+        return res.json({ error: 'invalid URL'});
+    }
+
+    
     const info = await ytdl.getBasicInfo(link);
     // const allFormats = info.formats;
     
@@ -25,7 +34,7 @@ app.post('/done', async (req, res) => {
     // res.send(filteredFormats);
     // res.send(data);
 
-    res.render('search', {data: data})
+    return res.render('search', {data: data})
 })
 
 app.post('/api', async (req, res) => {
